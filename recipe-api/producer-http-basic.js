87ce7ecef8f8f48aef761ad43cc79301876e66eb
LordFarron <brandon.brunno@gmail.com>
2021-11-20 21:50:33 -0300

    Add producer-consumer

---
+++ recipe-api/producer-http-basic.js
@@ -0,0 +1,32 @@
+import Fastify from 'fastify';
+const server = Fastify({logger:true});
+const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 4000;
+
+
+
+console.log(`worker pid = ${process.pid}`);
+
+server.get('/recipes/:id', async(req,reply)=>{
+    console.log(`worker request pid=${process.pid}`);
+    const id = Number(req.params.id);
+    if(id!=42){
+        reply.statusCode = 404;
+        return {error: 'not_found'};
+    }
+    return {
+        producer_pid: process.pid,
+        recipe: {
+            id, name: "Chicken Tikka Masala",
+            steps: "Throw it in a pot...",
+            igredients: [
+                {id: 1, name: "Chicken", quantity: "1 lb"},
+                {id: 2, name: "Sauce", quantity: "2 cups"}
+            ]
+        }
+    }
+})
+
+server.listen(PORT,HOST, ()=> {
+    console.log(`Producer running at http://${HOST}:${PORT}`);
+})
